7c7c0d2482739312f4cb5cc6b71c6b203b57ef86
Ana-Timoteo <a.timoteo@campus.fct.unl.pt>
2025-04-07 17:22:17 +0100

    fix Sidebar Account image

--- iscf-app/components/Sidebar/AccountToggle.tsx
+++ iscf-app/components/Sidebar/AccountToggle.tsx
@@ -6,10 +6,12 @@ export const AccountToggle = () => {
   return (
     <div className="border-b mb-4 mt-2 pb-4 border-stone-300">
       <button className="flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center">
-        <Image
-          src="https://api.dicebear.com/9.x/notionists/svg"
+      <Image
+          src="https://api.dicebear.com/9.x/notionists/svg?seed=account"
           alt="avatar"
-          className="size-8 rounded shrink-0 bg-violet-500 shadow"
+          width={32}
+          height={32}
+          className="rounded bg-violet-500 shadow"
         />
         <div className="text-start">
           <span className="text-sm font-bold block">XX Is Loading</span>
