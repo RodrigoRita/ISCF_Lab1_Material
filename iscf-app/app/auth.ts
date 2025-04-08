747d90977abb54f6ba64de9754fa5a592f872bd9
RodrigoRita <rs.rita@campus.fct.unl.pt>
2025-04-08 16:13:29 +0100

    Fix auth.ts

--- iscf-app/app/auth.ts
+++ iscf-app/app/auth.ts
@@ -1,16 +1,12 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      authorization: {
-        params: {
-          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`
-        }
-      }
     })
   ],
 })
+
