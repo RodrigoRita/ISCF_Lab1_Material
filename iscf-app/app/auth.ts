c7e7a5650693ed6a16bc346d4a38440736ac6b69
RodrigoRita <rs.rita@campus.fct.unl.pt>
2025-04-08 17:01:53 +0100

    Test auth.ts

--- iscf-app/app/auth.ts
+++ iscf-app/app/auth.ts
@@ -1,14 +1,20 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    })
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",  // Explicitly request the authorization code
+          code_challenge_method: "", // This disables PKCE
+        },
+      },
+    }),
   ],
- secret: process.env.NEXTAUTH_SECRET,  // Ensure this is passed
- debug: process.env.NODE_ENV === "development",  // For logging in dev
-});
-
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
