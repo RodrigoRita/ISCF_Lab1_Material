7c2987ff08f00e0be09179059d53c8850efdfc55
RodrigoRita <rs.rita@campus.fct.unl.pt>
2025-04-08 14:48:51 +0100

    Test auth.ts

--- iscf-app/app/auth.ts
+++ iscf-app/app/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
+console.log(process.env.NEXTAUTH_URL)
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
